d59507b0431675fc6c6c7e1cb7c51f149798bdc0
Vinicius Gomes <viniplay0599@gmail.com>
2023-02-08 17:06:22 -0300

    feat: implements routes and styled-components config

---
+++ src/services/api.ts
@@ -0,0 +1,7 @@
+export const getPokemons = async (limit: number, offset: number) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  );
+  const data = await response.json();
+  return data;
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,9 @@
+export default {
+  title: "light",
+  colors: {
+    primary: "#ef5350",
+
+    background: "#f0f0f5",
+    text: "#333",
+  },
+}
